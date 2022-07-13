03dd7ebc5a4f16a6fba2abc81cbbfa3efdaedea9
David <dyoungblood@skillstorm.com>
2022-07-13 18:20:25 -0400

    First Push
    Working code can resort back to this

---
+++ pj1/src/main/java/com/skillstorm/beans/Jdbc.java
@@ -0,0 +1,47 @@
+/*
+ * package com.skillstorm.beans;
+ * 
+ * import java.sql.SQLException; import java.util.HashSet; import java.util.Set;
+ * 
+ * import com.skillstorm.dao.ExpenseDAO; import
+ * com.skillstorm.dao.ReimbursementDAO;
+ * 
+ * public class Jdbc { public static void main(String[] args) throws
+ * SQLException {
+ * 
+ * 
+ * String url = "jdbc:mysql://localhost:3306/reimbursement"; String username =
+ * "root"; String password = "root";
+ * 
+ * 
+ * Connection conn = DriverManager.getConnection(url, username, password);
+ * 
+ * 
+ * 
+ * 
+ * createInstance(); updateReimbursement(2, "Approved"); getAll();
+ * 
+ * //conn.close(); System.out.println("DONE"); }
+ * 
+ * public static void createInstance() { try { ExpenseDAO dao = new
+ * ExpenseDAO(); dao.create(new Expense("Test", "Because"));
+ * 
+ * } catch (SQLException e) { // TODO Auto-generated catch block
+ * 
+ * e.printStackTrace(); } }
+ * 
+ * public static void updateReimbursement(int id, String str) { try {
+ * ReimbursementDAO dao = new ReimbursementDAO(); dao.update(id, str);
+ * 
+ * 
+ * } catch (SQLException e) { // TODO Auto-generated catch block
+ * 
+ * e.printStackTrace(); } }
+ * 
+ * public static Set<Expense> getAll() { Set<Expense> expenseSet = new
+ * HashSet<>(); try { ExpenseDAO dao = new ExpenseDAO(); expenseSet =
+ * dao.findAll(); } catch (Exception e) { System.out.println("Catch error"); }
+ * return expenseSet; }
+ * 
+ * }
+ */
---
+++ pj1/src/main/java/com/skillstorm/beans/Reimbursement.java
@@ -0,0 +1,64 @@
+package com.skillstorm.beans;
+
+import java.util.Objects;
+
+public class Reimbursement {
+
+	private int id;
+	private String status;
+
+	public Reimbursement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Reimbursement(int id, String status) {
+		super();
+		this.id = id;
+		this.status = status;
+	}
+
+	public Reimbursement(String status) {
+		super();
+		this.status = status;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Reimbursement [id=" + id + ", status=" + status + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, status);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reimbursement other = (Reimbursement) obj;
+		return id == other.id && Objects.equals(status, other.status);
+	}
+
+}

46838dcc8144284a67c229dabb7f880842f2c118
David <dyoungblood@skillstorm.com>
2022-07-13 19:28:46 -0400

    Style

--- npmpj1/src/App.js
+++ npmpj1/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -6,19 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 >Project 1</h1>
       </header>
+      <h1 className = "App-navigation">Navigation</h1>
+      <p className = "App-mainContent">Hello</p>  
     </div>
+
   );
 }
 

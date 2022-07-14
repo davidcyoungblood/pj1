2a7b492293b1754d04d4a4558f162270d5faf2b7
David <dyoungblood@skillstorm.com>
2022-07-13 22:57:57 -0400

    unset button css

--- npmpj1/src/App.jsx
+++ npmpj1/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button className='home-button'><img src={logo} className="App-logo" alt="logo" /></button>
+        
         <h1 >Project 1</h1>
       </header>
       <h1 className = "App-navigation">Navigation</h1>
